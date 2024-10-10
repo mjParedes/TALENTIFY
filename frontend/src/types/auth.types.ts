0669646a941d1e8461b20a80c76fcb441f977499
Vladimir Infante <infantevladimir@gmail.com>
2024-10-10 16:09:19 -0500

    feat: updated auth types

--- frontend/src/types/auth.types.ts
+++ frontend/src/types/auth.types.ts
@@ -1,12 +1,9 @@
 import type { z } from "zod";
-import type {
-  loginSchema,
-  registrationSchema,
-} from "@/validations/auth.schema";
+import type { loginSchema, registerSchema } from "@/validations/auth.schema";
 
 export type LoginDto = z.infer<typeof loginSchema>;
 
 export type RegisterDto = Omit<
-  z.infer<typeof registrationSchema>,
+  z.infer<typeof registerSchema>,
   "passwordConfirmation"
 >;
