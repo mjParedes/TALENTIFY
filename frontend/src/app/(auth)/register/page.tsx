47c5654f546c93171e950f3c4349d4ab9cf93885
Vladimir Infante <infantevladimir@gmail.com>
2024-10-10 16:15:06 -0500

    feat: added register form to register page

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,3 +1,9 @@
+import { RegisterForm } from "@/components/organisms/RegisterForm/RegisterForm";
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="flex w-full items-center justify-center pt-20">
+      <RegisterForm />
+    </div>
+  );
 }
