3eae968212493dfbb1a3fd1f5b0ae3b42742956b
Vladimir Infante <infantevladimir@gmail.com>
2024-10-09 19:02:40 -0500

    feat: updated buttons styles

--- frontend/src/components/atoms/Button/Button.styles.ts
+++ frontend/src/components/atoms/Button/Button.styles.ts
@@ -1,17 +1,18 @@
 import { cva } from "class-variance-authority";
 
 export const buttonVariants = cva(
-  "rounded-md border text-center text-sm transition-all shadow-md hover:shadow-lg focus:shadow-none active:shadow-none",
+  "rounded-full border text-white flex font-semibold items-center duration-300 justify-center text-sm transition-all shadow-md hover:shadow-lg focus:shadow-none active:shadow-none",
   {
     variants: {
       variant: {
         default: "",
         primary:
-          "text-white border-transparent bg-cyan-600 active:bg-cyan-700 hover:bg-cyan-700",
+          "border-transparent bg-cyan-600 active:bg-cyan-700 hover:bg-cyan-700",
         secondary:
           "bg-transparent border-white hover:border-neutral-300 hover:text-neutral-300 active:text-neutral-300 active:border-neutral-300",
         disabled:
           "pointer-events-none opacity-50 shadow-none text-neutral-600 bg-neutral-400",
+        outlined: "border-cyan-600 bg-white hover:bg-cyan-200 text-cyan-600",
       },
       size: {
         default: "h-10 px-4",
