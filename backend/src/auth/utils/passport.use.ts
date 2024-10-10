7d4390a93946e4d098e2e447d7cefcd19940b7d4
Matias Paredes <tutecapo1985@gmail.com>
2024-10-10 19:59:16 -0300

    refactor(auth): refactor strategies and create authentication service

--- backend/src/auth/utils/passport.use.ts
+++ backend/src/auth/utils/passport.use.ts
@@ -1,12 +1,9 @@
-import passport, { Strategy } from "passport";
+import passport, { Strategy } from 'passport';
 
-type TypeStrategy<T, U, X> = { new(params: U, callback: X): T }
-
-export function PassportUse<T extends Strategy, U, X>(
+export function PassportUse<T extends Strategy>(
     name: string,
-    Strategy: TypeStrategy<T, U, X>,
-    params: U,
-    callback: X) {
-    passport.use(name, new Strategy(params, callback));
+    Strategy: new (...args: any[]) => T,
+    ...args: ConstructorParameters<typeof Strategy>
+) {
+    passport.use(name, new Strategy(...args));
 }
-
