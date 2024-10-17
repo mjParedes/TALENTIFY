f886194644188cf4a4e905eac06a318786a79988
David Castro <davidernesto.castroanaya@gmail.com>
2024-10-17 09:03:20 -0600

    index.ts update

---
+++ backend/src/assets/response.text.ts
@@ -0,0 +1,26 @@
+// responseText.ts
+const responseText = `
+  Welcome to TALENTIFY.COM - We make it happen!<br><br>
+
+  Available Endpoints:<br><br>
+  
+  1. /api/auth<br>
+     - Description: Authentication routes (login, register, etc.)<br>
+     - Methods: GET, POST, PUT, DELETE<br><br>
+  
+  2. /api/users<br>
+     - Description: User management (create, read, update, delete users)<br>
+     - Methods: GET, POST, PUT, DELETE<br><br>
+  
+  3. /api/company<br>
+     - Description: Company management (create, read, update, delete companies)<br>
+     - Methods: GET, POST, PUT, DELETE<br><br>
+  
+  4. /api/profiles<br>
+     - Description: Profile management (create, read, update, delete profiles)<br>
+     - Methods: GET, POST, PUT, DELETE<br><br>
+
+  Note: Full CRUD is available for each endpoint.<br>
+`;
+
+export default responseText;
