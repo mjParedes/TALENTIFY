ede2bd4eac2a724813a407ed69328745bedb8750
Matias Paredes <tutecapo1985@gmail.com>
2024-10-19 20:55:38 -0300

    feat(auth): implement route validation

--- backend/src/routes/favorite.route.ts
+++ backend/src/routes/favorite.route.ts
@@ -1,20 +1,21 @@
 import { Router } from "express";
 import { FavoriteController } from '../controllers/favorite.controller';
+import { isAuth } from "../helpers/authenticate.helper";
 
 const favoriteController = new FavoriteController()
 
 const router = Router()
 
-router.post('/', (req, res) => favoriteController.createFavorite(req, res))
+router.post('/', isAuth, (req, res) => favoriteController.createFavorite(req, res))
 
-router.get('/', (req, res) => favoriteController.getAllFavorites(req, res))
+router.get('/', isAuth, (req, res) => favoriteController.getAllFavorites(req, res))
 
-router.get('/:id', (req, res) => favoriteController.getFavoriteById(req, res))
+router.get('/:id', isAuth, (req, res) => favoriteController.getFavoriteById(req, res))
 
-router.put('/:id', (req, res) => favoriteController.updateFavorite(req, res))
+router.put('/:id', isAuth, (req, res) => favoriteController.updateFavorite(req, res))
 
-router.delete('/:id', (req, res) => favoriteController.deleteFavorite(req, res))
+router.delete('/:id', isAuth, (req, res) => favoriteController.deleteFavorite(req, res))
 
-router.delete('/', (req, res) => favoriteController.deleteAllFavorites(req, res))
+router.delete('/', isAuth, (req, res) => favoriteController.deleteAllFavorites(req, res))
 
 export default router
--- backend/src/routes/offer.route.ts
+++ backend/src/routes/offer.route.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import { OfferController } from "../controllers/offer.controller";
+import { isAuth } from "../helpers/authenticate.helper";
 
 const offerController = new OfferController()
 
 const router = Router()
 
-router.post('/', (req, res) => offerController.createOffer(req, res))
+router.post('/',isAuth, (req, res) => offerController.createOffer(req, res))
 
-router.get('/', (req, res) => offerController.getAllOffers(req, res))
+router.get('/',isAuth, (req, res) => offerController.getAllOffers(req, res))
 
-router.get('/:id', (req, res) => offerController.getOfferById(req, res))
+router.get('/:id',isAuth, (req, res) => offerController.getOfferById(req, res))
 
-router.put('/:id', (req, res) => offerController.updateOffer(req, res))
+router.put('/:id',isAuth, (req, res) => offerController.updateOffer(req, res))
 
-router.delete('/:id', (req, res) => offerController.deleteOffer(req, res))
+router.delete('/:id',isAuth, (req, res) => offerController.deleteOffer(req, res))
 
 export default router
