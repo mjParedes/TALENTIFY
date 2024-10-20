6b907fc07938bf8cf7f8313d721dccf3915db218
Matias Paredes <tutecapo1985@gmail.com>
2024-10-20 02:15:47 -0300

    fix(auth): solving isAuth issues

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -2,13 +2,10 @@ import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
 import { AuthService } from "../services/auth.service";
 
-// Crear una instancia del AuthService
 const authService = new AuthService();
 
-// Crear una instancia del AuthController y pasar AuthService
 const authController = new AuthController(authService);
 
-// Crear una instancia de Express Router
 const router = Router();
 
 // Función auxiliar para manejar controladores asíncronos y capturar errores
@@ -16,12 +13,9 @@ const asyncHandler = (fn: any) => (req: any, res: any, next: any) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-// Ruta para el registro de usuarios (usamos asyncHandler para manejar errores)
-router.post("/signup", asyncHandler((req:any, res:any) => authController.signup(req, res)));
+router.post("/signup", asyncHandler((req: any, res: any) => authController.signup(req, res)));
 
-// Ruta para el login de usuarios
-router.post("/login", asyncHandler((req:any, res:any) => authController.login(req, res)));
+router.post("/login", asyncHandler((req: any, res: any) => authController.login(req, res)));
 
-// Exportar las rutas
 export default router;
 
