1c17b4a37aa40fbce6a1d5445cec9db3e06cefc4
Vladimir Infante <infantevladimir@gmail.com>
2024-10-21 11:32:35 -0500

    feat: updated auth types

--- frontend/src/types/auth.types.ts
+++ frontend/src/types/auth.types.ts
@@ -7,3 +7,18 @@ export type RegisterDto = Omit<
   z.infer<typeof registerSchema>,
   "passwordConfirmation"
 >;
+
+type Roles = "USER" | "RECRUITER" | "ADMIN";
+export interface UserDataDto {
+  id: number;
+  email: string;
+  fullName: string;
+  password: string;
+  role: Roles;
+  companyId: number | null;
+}
+
+export interface LoginResponseDto {
+  accessToken: string;
+  user: UserDataDto;
+}
