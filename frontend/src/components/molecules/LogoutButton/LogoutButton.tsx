b163d6a383d54b15b5bea18a903649898cecb02a
Vladimir Infante <infantevladimir@gmail.com>
2024-10-21 12:10:26 -0500

    feat: now logout button implements logout hook

--- frontend/src/components/molecules/LogoutButton/LogoutButton.tsx
+++ frontend/src/components/molecules/LogoutButton/LogoutButton.tsx
@@ -1,17 +1,21 @@
-import Link from "next/link";
+"use client";
+
 import { LogOut } from "lucide-react";
 import { Button } from "@/components/atoms/Button/Button";
+import { useLogout } from "@/services/mutations/auth-mutations";
 
 export const LogoutButton = () => {
+  const logOut = useLogout();
   return (
     <div className="flex w-full flex-col items-center">
       <Button
         variant="textWithIcon"
         size="medium"
         className="mt-4 flex w-full justify-start pr-4"
+        onClick={logOut}
       >
         <LogOut className="mx-2" />
-        <Link href={"/sign-out"}>Cerrar sesión</Link>
+        Cerrar sesión
       </Button>
     </div>
   );
