9bc0d7ccc337af0cfddc9c0e131e358b0eb97897
Vladimir Infante <infantevladimir@gmail.com>
2024-10-21 12:17:00 -0500

    feat: added page and welcome message after login

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { WelcomeMessage } from "@/components/molecules/WelcomeMessage/WelcomeMessage";
+
+export default function Dashboard() {
+  return (
+    <main className="w-full pt-[25%] text-center">
+      <WelcomeMessage />
+    </main>
+  );
+}
---
+++ frontend/src/components/molecules/WelcomeMessage/WelcomeMessage.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useUserStore } from "@/store/auth.store";
+import { Text } from "../../atoms/Text/Text";
+
+export function WelcomeMessage() {
+  const user = useUserStore((state) => state.user);
+  return (
+    <Text variant="Headline-1/32">
+      Bienvenido {user ? user.fullName : "Usuario"}
+    </Text>
+  );
+}
