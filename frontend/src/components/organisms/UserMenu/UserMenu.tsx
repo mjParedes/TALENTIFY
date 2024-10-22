585d61163b75107252e117cb7adf2facc2cbfb9d
Vladimir Infante <infantevladimir@gmail.com>
2024-10-22 12:10:44 -0500

    feat: updated userMenu component to use new stores and hooks

--- frontend/src/components/organisms/UserMenu/UserMenu.tsx
+++ frontend/src/components/organisms/UserMenu/UserMenu.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { useCallback } from "react";
 import { X } from "lucide-react";
 import { InfoUser } from "@/components/molecules/InfoUser/InfoUser";
 import { LogoutButton } from "@/components/molecules/LogoutButton/LogoutButton";
 import { MenuUserButtons } from "@/components/molecules/MenuUserButtons/MenuUserButtons";
+import { useClickOutside } from "@/hooks/useClickOutside";
+import { useUserMenuStore } from "@/store/userMenu.store";
 
 export const UserMenu = () => {
+  const closeMenu = useUserMenuStore((state) => state.closeMenu);
+
+  const handleClickOutsideOrEscape = useCallback(() => {
+    closeMenu();
+  }, []);
+
+  const ref = useClickOutside<HTMLDivElement>(handleClickOutsideOrEscape);
+
   return (
-    <div className="absolute -right-3 top-14 z-10 flex h-full w-full flex-col items-start justify-between rounded-3xl bg-white p-4 md:h-[28.875rem] md:w-[21.25rem] md:border-[1px] md:border-solid md:border-gray-500">
+    <div
+      ref={ref}
+      className="absolute -right-3 top-14 z-10 flex h-full w-full flex-col items-start justify-between rounded-3xl bg-white p-4 md:h-[28.875rem] md:w-[21.25rem] md:border-[1px] md:border-solid md:border-gray-500"
+    >
       <Link className="self-end text-gray-900 md:hidden" href="/">
         <X />
       </Link>
