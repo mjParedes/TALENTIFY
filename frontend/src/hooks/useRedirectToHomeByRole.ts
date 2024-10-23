fb43a9dff4030b2d3d55bfb6bb31c348b4d580aa
Vladimir Infante <infantevladimir@gmail.com>
2024-10-23 08:45:34 -0500

    feat: added hook to redirect users by role

---
+++ frontend/src/hooks/useRedirectToHomeByRole.ts
@@ -0,0 +1,26 @@
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useUserStore } from "@/store/auth.store";
+
+export function useRedirectToHomeByRole() {
+  const router = useRouter();
+  const userRole = useUserStore((state) => state.user?.role);
+
+  useEffect(() => {
+    if (!userRole) {
+      return;
+    }
+    if (
+      userRole === "RECRUITER" &&
+      window?.location?.pathname.startsWith("/home/users")
+    ) {
+      router.push("/home/recruiters");
+    }
+    if (
+      userRole === "USER" &&
+      window?.location?.pathname.startsWith("/home/recruiters")
+    ) {
+      router.push("/home/users");
+    }
+  }, [userRole]);
+}
