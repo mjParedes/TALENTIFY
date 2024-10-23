cc72c9619ae3090c7c159605f2aaeb9c0442f47c
jime <jimemiramontes@gmail.com>
2024-10-23 10:23:10 -0300

    feat(studies)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
-import responseText from './assets/response.text';
-import authRoute from './routes/auth.route'
-import userRoute from "./routes/user.route"
-import companyRoute from './routes/company.route'
-import offerRoute from './routes/offer.route'
-import profileRoute from './routes/profile.route'
-import experienceRoute from './routes/experience.route'
-
+import cors from "cors";
+import responseText from "./assets/response.text";
+import authRoute from "./routes/auth.route";
+import userRoute from "./routes/user.route";
+import companyRoute from "./routes/company.route";
+import offerRoute from "./routes/offer.route";
+import profileRoute from "./routes/profile.route";
+import experienceRoute from "./routes/experience.route";
+import studiesRoute from "./routes/studies.route";
 
 dotenv.config();
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-}))
-app.use(express.json())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+);
+app.use(express.json());
 
-
-app.use('/api/auth', authRoute)
-app.use('/api/users', userRoute);
-app.use('/api/company', companyRoute)
-app.use('/api/offers', offerRoute)
-app.use('/api/profiles', profileRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/company", companyRoute);
+app.use("/api/offers", offerRoute);
+app.use("/api/profiles", profileRoute);
 // app.use('/api/applications')
-app.use('/api/experience', experienceRoute)
-
+app.use("/api/experience", experienceRoute);
+app.use("/api/studies", studiesRoute);
 
 app.get("/api/", (req, res) => {
   res.send(responseText);
 });
 
 const port = process.env.PORT || 3000;
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || "development";
 app.listen(port, () => {
-  if (environment === 'production') {
+  if (environment === "production") {
     console.log(`[server]: Server is running at the Railway public URL`);
   } else {
     console.log(`[server]: Server is running at http://localhost:${port}/api/`);
