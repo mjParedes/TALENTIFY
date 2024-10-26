ecec0939703249f003fb204e44df54a0d93c386e
Vladimir Infante <infantevladimir@gmail.com>
2024-10-26 17:49:51 -0500

    feat: updated table atom styles

--- frontend/src/components/atoms/Table/Table.tsx
+++ frontend/src/components/atoms/Table/Table.tsx
@@ -90,7 +90,10 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn(
+      "min-w-max p-4 align-middle [&:has([role=checkbox])]:pr-0",
+      className
+    )}
     {...props}
   />
 ));
