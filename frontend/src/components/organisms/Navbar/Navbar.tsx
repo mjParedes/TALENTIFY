fbb12159d4cc6307482d853738f2e5989747534f
Vladimir Infante <infantevladimir@gmail.com>
2024-10-29 08:36:30 -0500

    feat: now navbar displays grows to sides in users/recruiters home page

--- frontend/src/components/organisms/Navbar/Navbar.tsx
+++ frontend/src/components/organisms/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useUserStore } from "@/store/auth.store";
 import { cn } from "@/utils/cn";
 import { PageLogo } from "../../atoms/PageLogo/PageLogo";
@@ -8,8 +9,10 @@ import { NavUserSection } from "../NavUserSection/NavUserSection";
 
 export function Navbar() {
   const user = useUserStore((state) => state.user);
+  const pathname = usePathname();
   const isRecruiter = user?.role === "RECRUITER";
 
+  const isDashboard = pathname.includes("/home");
   const homePathByRole = isRecruiter ? "/home/recruiters" : "/home/users";
 
   return (
@@ -19,7 +22,12 @@ export function Navbar() {
         isRecruiter && "bg-violet-900"
       )}
     >
-      <div className="mx-auto flex w-[95%] max-w-7xl justify-between py-4">
+      <div
+        className={cn(
+          "mx-auto flex w-[90%] justify-between py-4",
+          !isDashboard && "w-[95%] max-w-7xl"
+        )}
+      >
         <Link
           href={user ? homePathByRole : "/"}
           className="flex h-[inherit] items-center text-center text-2xl font-bold text-purple-700"
